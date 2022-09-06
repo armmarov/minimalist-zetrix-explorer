04c9647477f37e1c56cf3045b9014110514483de
Ammar <armmarov@gmail.com>
2022-09-06 08:11:23 +0800

    Fix env variable

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-BASEURL='http://18.136.67.108:19333' PORT=3000 pm2 start npm --name relay -- start
-BASEURL='http://13.250.42.42:19333' PORT=3001 pm2 start npm --name testnet -- start
-BASEURL='http://13.250.42.42:18002' PORT=3002 pm2 start npm --name mainnet -- start
+REACT_APP_BASEURL='http://18.136.67.108:19333' PORT=3000 pm2 start npm --name relay -- start
+REACT_APP_BASEURL='http://13.250.42.42:19333' PORT=3001 pm2 start npm --name testnet -- start
+REACT_APP_BASEURL='http://13.250.42.42:18002' PORT=3002 pm2 start npm --name mainnet -- start
